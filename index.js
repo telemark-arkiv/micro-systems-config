ead322faed1ee74b9e1774b657c7f74221cd7b4f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-06 09:36:55 +0100

    Moves systems to config (patch)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { send } = require('micro')
-const systems = require('./systems')
+const systems = require('./config/systems.json')
 
 module.exports = async (request, response) => {
   send(response, 200, systems)
